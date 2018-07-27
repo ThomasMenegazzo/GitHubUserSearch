82cf92df3530e94ae746855b2f959a699242bb01
Thomas <thomasmenegazzo@gmail.com>
2018-07-27 20:39:46 -0300

    Implementação do sistema de autorização

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+var request = require('superagent');
 
 var app = express();
+var authToken = null;
+var clientID = '6dabb1b6637c94ddd7b0';
+var  clientSecret = '2d6e6357d90075eac7976ec647e24a12ab439a63';
 
 //Define engine e pasta para o HTML
 app.set('view engine','ejs');
@@ -18,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 //Rota para busca de usuário no GitHub através do index
 app.post('/user', function(req, res){
   var newUser = req.body.userName;
+  if (authToken){newUser = `${newUser}?access_token=${authToken}`}
   res.render('user', {newUser: newUser});
 });
 
@@ -28,7 +33,9 @@ app.post('/repository', function(req, res){
 
 //Rota para busca de usuário no GitHub através da URL
 app.get('/user/:newUser', function(req, res){
-  res.render('user', req.params);
+  newUser = req.params.newUser;
+  if (authToken){newUser = `${newUser}?access_token=${authToken}`}
+  res.render('user', {newUser: newUser});
 });
 
 //rota direta para a pasta de repositório (retorna ao ultimo repositório visitado)
@@ -36,9 +43,25 @@ app.get('/repository', function(req, res){
   res.render('repository');
 });
 
+app.get('/authorized', function(req, res){
+  authCode = req.query.code;
+  request
+    .post('https://github.com/login/oauth/access_token')
+    .send({ 
+      client_id: clientID,
+	 client_secret: clientSecret,
+    code: authCode})
+    .set('Accept', 'application/json')
+    .then(response => {
+	 authToken=response.body.access_token;
+    }).catch(err => {console.log('ERROR');}
+	 );
+  res.render('index', {clientID: clientID, authorized: true});
+});
+
 //Rota inicial
 app.get('/', function(req, res){
-  res.render('index');
+  res.render('index', {clientID: clientID, authorized: false});
 });
 
 //Inicialização do server
